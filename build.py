d3c005b240a3e91e85dd3739d6d3a7a83f7b43eb
Alden Deran <aldenderan@protonmail.com>
2023-02-10 23:35:08 -0800

    -

--- build.py
+++ build.py
@@ -7,18 +7,27 @@ import intelliwaterai.infinitydb.access as idb
 
 INTERFACE = "com.boilerbay.genomics"
 
+def make_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--server", default="https://localhost:37411/infinitydb/data")
+    parser.add_argument("--database", type=str, default="boilerbay/genomics")
+    parser.add_argument("--user", type=str, default="ai")
+    parser.add_argument("--password", type=str, default="ai")
+    return parser
+
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = make_parser()
     parser.add_argument("--fasta")
     parser.add_argument("--genome_id")
     args = parser.parse_args()
 
     server = idb.InfinityDBAccessor(
-        server_url = "http://localhost:37411/infinitydb/data",
-        db = "boilerbay/genomics",
-        user="ai",
-        password="ai"
+        server_url = args.server,
+        db = args.database,
+        user = args.user,
+        password = args.password
     )
 
     with open(args.fasta) as fh:
