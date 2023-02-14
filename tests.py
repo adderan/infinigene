40a0539e230b7ec1b6fb94b0dbb832ea574b5555
Alden Deran <aldenderan@protonmail.com>
2023-02-14 11:39:13 -0800

    -

--- tests.py
+++ tests.py
@@ -3,25 +3,32 @@ from build import make_parser
 import intelliwaterai.infinitydb.access as idb
 from build import INTERFACE
 
+GENOME = idb.Attribute("genome")
+CHROMOSOME = idb.Attribute("chromosome")
+POSITION = idb.Attribute("position")
+BASE = idb.Attribute("base")
 
 class QueryTests(unittest.TestCase):
     def setUp(self):
         self.server = idb.InfinityDBAccessor(
-            server_url = "https://localhost:37411/infinitydb/data",
+            server_url = "https://24.6.93.122:37411/infinitydb/data",
             db = "boilerbay/genomics",
             user = "ai",
             password = "ai"
         )
 
 
+
     def test_set_bases(self):
+        data = {
+            (GENOME, "dinosaur", CHROMOSOME, "chr1", POSITION, 0, BASE, "A"): None,
+            (GENOME, "dinosaur", CHROMOSOME, "chr1", POSITION, 1, BASE, "C"): None,
+            (GENOME, "dinosaur", CHROMOSOME, "chr1", POSITION, 2, BASE, "T"): None
+        }
+        print(data)
+
         self.server.execute_query(
-            prefix = [INTERFACE, "set_bases"],
-            data = {
-                idb.Attribute("genome"): "dinosaur",
-                idb.Attribute("chromosome"): "chr1",
-                idb.Attribute("start"): 0,
-                idb.Attribute("length"): 10,
-                idb.Attribute("bases"): "ACTAGGACAGT"
-            }
+            prefix = [INTERFACE, "set_base"],
+            data = data,
+            compact_tips=False
         )
