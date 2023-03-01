7ed3b48bcff6bf709962ec5565dbe04f07389888
Alden Deran <aldenderan@protonmail.com>
2023-03-01 13:29:40 -0800

    -

--- browser.py
+++ browser.py
@@ -1,9 +1,9 @@
+import argparse
 import intelliwaterai.infinitydb.access as idb
 from queries import get_sequence
 from flask import Flask, render_template, request, url_for
 
-server = idb.InfinityDBAccessor("https://24.6.93.122:37411/infinitydb/data", db="boilerbay/genomics", user="ai", password="ai")
-server.is_verification_enabled = False
+server = None
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
@@ -24,4 +24,23 @@ def home():
     seq = get_sequence(server, genome="grch38", chromosome=chromosome, start=start, end=end)
     seq = "\t".join(seq)
     return render_template("genome_browser.html", seq=seq)
+
+@app.route("/sequence/<genome>/<chromosome>/<int:start>/<int:end>")
+def getSequence(genome, chromosome, start, end):
+    seq = get_sequence(server, genome=genome, chromosome=chromosome, start=start, end=end)
+    return seq
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--server", type=str, default="https://localhost:37411/infinitydb/data")
+    parser.add_argument("--db", type=str, default="boilerbay/genomics")
+    parser.add_argument("--user", type=str, default="ai")
+    parser.add_argument("--password", type=str, default="ai")
+    args = parser.parse_args()
+
+    server = idb.InfinityDBAccessor(server_url=args.server, db=args.db, user=args.user, password=args.password)
+    server.is_verification_enabled = False
+
+    app.run()
 
--- static/genome_browser.js
+++ static/genome_browser.js
@@ -5,4 +5,14 @@ ctx.fillStyle = "green";
 
 ctx.fillRect(10, 10, 150, 150);
 
-console.log(SCRIPT_ROOT);
+let seq_url = `${SCRIPT_ROOT}/sequence/grch38/chr1/1000000/1000100`;
+
+//let seq = fetch(seq_url).then(response => response.text()).then(response => document.write(response));
+
+go_button = document.getElementById("go");
+
+function display_sequence() {
+    fetch(seq_url).then(response => response.text()).then(response => alert(response));
+};
+go_button.onclick = display_sequence;
+
