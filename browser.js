5f9f55f105d4f3761ae31fb9075ee06708cb3096
Alden Deran <aldenderan@protonmail.com>
2023-03-02 19:45:04 -0800

    -

--- browser.js
+++ browser.js
@@ -7,49 +7,58 @@ class IdbAccessor {
         this.username = username
         this.password = password
         this.db_url = this.server_url + "/" + this.db
-        this.headers = new Headers();
 
-        let auth = this.username + ":" + this.password
-        auth = btoa(auth)
-
-        console.log(auth)
-        this.headers.append('Authorization', "Basic " + auth)
-        //this.headers.append("Content-Type", "application/json");
+        this.auth = btoa(this.username + ":" + this.password)
 
     }
     
     do_query(prefix, data) {
+        let headers = new Headers()
+        headers.append('Authorization', "Basic " + this.auth)
+        headers.append("Content-Type", "application/json");
+
         let query_url = this.db_url + '/' + prefix.join('/')
         const request = new Request(query_url,
             {
-                headers: this.headers,
+                headers: headers,
                 method: "POST",
-                body: JSON.stringify(data)
+                body: JSON.stringify(data),
+                credentials: "include"
             })
 
         return fetch(request);
     }
 
     head() {
+        let headers = new Headers()
+        headers.append('Authorization', "Basic " + this.auth)
+
         const request = new Request(this.db_url,
             {
-                headers: this.headers,
+                headers: headers,
                 method: "HEAD",
-            })
+                credentials: "include"
+            });
+
         fetch(request).then((response) => console.log("head: " + response));
     }
 
 }
 
-
+/*
 server = new IdbAccessor("https://infinitydb.com:37411/infinitydb/data", "ai/labels", "ai", "ai")
 server.head()
 
-server.do_query(["com.infinitydb.ai", "get_first_image_id"], 
+server.do_query(["com.infinitydb.ai", "get_first_image_id"],
     data = {
         "image_set": "chlorella"
     }
-);
+)
+*/
+
+server = new IdbAccessor("https://localhost:37411/infinitydb/data", "boilerbay/genomics", "ai", "ai")
+server.head()
+
 
 server.do_query(["com.boilerbay.genomics", "get_sequence"], 
     data={
